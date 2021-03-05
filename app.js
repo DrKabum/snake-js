7b8ded12b962ee5c39bcfc9c6e7d7200d2773a47
Nicolas Broca <n.broca@gmail.com>
2021-03-05 21:35:19 +0100

    add apple management, refactor isGameOver
    Apple are now randomly generated on the grid, eating them is taken care of, and also refactored Game over state to account for the snake eating itself

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ let squares = []
 let currentSnake = [2, 1, 0]
 let direction = 1
 let width = 10
-let time = 1000
+let time = 600
+let timeRise = .05 // in %
+let appleIndex = 0
+let applePoints = 1
+let score = 0
 
 // Initialize game
 function createGrid() {
@@ -22,6 +26,7 @@ function createGrid() {
 
 createGrid()
 currentSnake.forEach(index => squares[index].classList.add('snake'))
+generateApple()
 
 // controls
 function move() {
@@ -29,6 +34,11 @@ function move() {
         const tail = currentSnake.pop(0)
         squares[tail].classList.remove('snake')
         const head = currentSnake.unshift(currentSnake[0] + direction)
+        if(squares[currentSnake[0]].classList.contains('apple')) {
+            eatApple(tail)
+            clearInterval(timerId)
+            timerId = setInterval(move, time)
+        }
         squares[currentSnake[0]].classList.add('snake')
     } else {
         clearInterval(timerId)
@@ -36,28 +46,47 @@ function move() {
     }    
 }
 
-const timerId = setInterval(move, time)
+let timerId = setInterval(move, time)
 
 function control(e) {
     //right
-    if (e.keyCode === 39 && direction !== -1) direction = 1
+    if (e.keyCode === 39) direction = 1
     // left
-    else if (e.keyCode === 37 && direction !== 1) direction = -1
+    else if (e.keyCode === 37) direction = -1
     // up
-    else if (e.keyCode === 38 && direction !== width) direction = -width
+    else if (e.keyCode === 38) direction = -width
     // down
-    else if (e.keyCode === 40 && direction !== -width) direction = width
+    else if (e.keyCode === 40) direction = width
+
 }
 
 document.addEventListener('keydown', control)
 
 // Game rules
-function isOutOfBoard() {
+function isGameOver() {
     return (currentSnake[0] + direction < 0 ||
-        currentSnake[0] + direction > 100 ||
-        (currentSnake[0] % 10 === 0 && direction === -1) ||
-        (currentSnake[0] % 10 === 9 && direction === 1)
+        currentSnake[0] + direction > width*width ||
+        (currentSnake[0] % width === 0 && direction === -1) ||
+        (currentSnake[0] % width === 9 && direction === 1) ||
+        squares[currentSnake[0] + direction].classList.contains('snake')
     )
 }
 
-function isGameOver() { return isOutOfBoard() }
+function generateApple() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake'))
+
+    squares[appleIndex].classList.add('apple')
+}
+
+function eatApple(tail) {
+    squares[appleIndex].classList.remove('apple')
+    currentSnake.push(tail)
+    squares[tail].classList.add('snake')
+    time -= (time*timeRise)
+    console.log(`${time} ms.`)
+    score += applePoints
+    scoreBoard.textContent = score
+    generateApple()
+}
