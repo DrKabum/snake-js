035ce5ec2e635aa05fdda2cd7aa6e930515f27fd
Nicolas Broca <n.broca@gmail.com>
2021-03-05 18:43:36 +0100

    add controls

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const grid = document.querySelector(".grid")
 const startBtn = document.getElementById("start")
 const scoreBoard = document.getElementById("score")
 
+// vars
 let squares = []
 let currentSnake = [2, 1, 0]
 let direction = 1
 let time = 1000
 
+// Initialize game
 function createGrid() {
     for(let i=0; i<100; i++) {
         const square = document.createElement('div')
@@ -18,9 +20,9 @@ function createGrid() {
 }
 
 createGrid()
-
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
+// controls
 function move() {
     const tail = currentSnake.pop(0)
     squares[tail].classList.remove('snake')
@@ -28,5 +30,17 @@ function move() {
     squares[currentSnake[0]].classList.add('snake')
 }
 
+function control(e) {
+    //right
+    if (e.keyCode === 39 && direction !== -1) direction = 1
+    // left
+    else if (e.keyCode === 37 && direction !== 1) direction = -1
+    // up
+    else if (e.keyCode === 38 && direction !== 10) direction = -10
+    // down
+    else if (e.keyCode === 40 && direction !== -10) direction = 10
+}
+
 const timerId = setInterval(move, time)
 
+document.addEventListener('keydown', control)
